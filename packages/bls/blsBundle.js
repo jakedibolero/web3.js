ee9a9d17113866a5f3cfc8d133865d6d07430676
Jeremiah Valero <jeremiah.v@scopicsoftware.com>
2021-04-20 14:54:38 +0800

    Integrated additional functions, moved directory

--- packages/bls/blsBundle.js
+++ packages/bls/blsBundle.js
@@ -2,52 +2,11 @@
 const keccak = require('keccak');
 const randomBytes = require('randombytes');
 const CryptoJS = require('crypto-js')
-let bls_ = require('./bls/bls');
-
+let bls_ = require('./bls');
+const v4 = require('uuid').v4;
 const step = 500;
 
 const  DefaultDataDirName = "petrichor";
-
-//
-var rng = require('rng');
-function bytesToUuid(buf, offset) {
-    var i = offset || 0;
-    var bth = byteToHex;
-    // join used to fix memory issue caused by concatenation: https://bugs.chromium.org/p/v8/issues/detail?id=3175#c4
-    return ([bth[buf[i++]], bth[buf[i++]], 
-      bth[buf[i++]], bth[buf[i++]], '-',
-      bth[buf[i++]], bth[buf[i++]], '-',
-      bth[buf[i++]], bth[buf[i++]], '-',
-      bth[buf[i++]], bth[buf[i++]], '-',
-      bth[buf[i++]], bth[buf[i++]],
-      bth[buf[i++]], bth[buf[i++]],
-      bth[buf[i++]], bth[buf[i++]]]).join('');
-  }
-
-function v4(options, buf, offset) {
-  var i = buf && offset || 0;
-
-  if (typeof(options) == 'string') {
-    buf = options === 'binary' ? new Array(16) : null;
-    options = null;
-  }
-  options = options || {};
-
-  var rnds = options.random || (options.rng || rng)();
-
-  // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
-  rnds[6] = (rnds[6] & 0x0f) | 0x40;
-  rnds[8] = (rnds[8] & 0x3f) | 0x80;
-
-  // Copy bytes to buffer, if provided
-  if (buf) {
-    for (var ii = 0; ii < 16; ++ii) {
-      buf[i + ii] = rnds[ii];
-    }
-  }
-
-  return buf || bytesToUuid(rnds);
-}
 
 /**
  * Transform a private key into an address
@@ -59,6 +18,149 @@ const signMessage = (hashMessage, secretBytes) => bls_.BonehLynnShacham.sign(has
 
 const verifyMessage = (publicKeyG2, hashedMessage, signedHashedMessageG1) => bls_.BonehLynnShacham.verify(publicKeyG2, hashedMessage, signedHashedMessageG1);
 
+
+function download(data, strFileName, strMimeType) {
+
+    var self = window, // this script is only for browsers anyway...
+        defaultMime = "application/octet-stream", // this default mime also triggers iframe downloads
+        mimeType = strMimeType || defaultMime,
+        payload = data,
+        url = !strFileName && !strMimeType && payload,
+        anchor = document.createElement("a"),
+        toString = function(a){return String(a);},
+        myBlob = (self.Blob || self.MozBlob || self.WebKitBlob || toString),
+        fileName = strFileName || "download",
+        blob,
+        reader;
+        myBlob= myBlob.call ? myBlob.bind(self) : Blob ;
+
+    if(String(this)==="true"){ //reverse arguments, allowing download.bind(true, "text/xml", "export.xml") to act as a callback
+        payload=[payload, mimeType];
+        mimeType=payload[0];
+        payload=payload[1];
+    }
+
+
+    if(url && url.length< 2048){ // if no filename and no mime, assume a url was passed as the only argument
+        fileName = url.split("/").pop().split("?")[0];
+        anchor.href = url; // assign href prop to temp anchor
+          if(anchor.href.indexOf(url) !== -1){ // if the browser determines that it's a potentially valid url path:
+            var ajax=new XMLHttpRequest();
+            ajax.open( "GET", url, true);
+            ajax.responseType = 'blob';
+            ajax.onload= function(e){ 
+              download(e.target.response, fileName, defaultMime);
+            };
+            setTimeout(function(){ ajax.send();}, 0); // allows setting custom ajax headers using the return:
+            return ajax;
+        } // end if valid url?
+    } // end if url?
+
+
+    //go ahead and download dataURLs right away
+    if(/^data:([\w+-]+\/[\w+.-]+)?[,;]/.test(payload)){
+
+        if(payload.length > (1024*1024*1.999) && myBlob !== toString ){
+            payload=dataUrlToBlob(payload);
+            mimeType=payload.type || defaultMime;
+        }else{			
+            return navigator.msSaveBlob ?  // IE10 can't do a[download], only Blobs:
+                navigator.msSaveBlob(dataUrlToBlob(payload), fileName) :
+                saver(payload) ; // everyone else can save dataURLs un-processed
+        }
+
+    }else{//not data url, is it a string with special needs?
+        if(/([\x80-\xff])/.test(payload)){			  
+            var i=0, tempUiArr= new Uint8Array(payload.length), mx=tempUiArr.length;
+            for(i;i<mx;++i) tempUiArr[i]= payload.charCodeAt(i);
+             payload=new myBlob([tempUiArr], {type: mimeType});
+        }		  
+    }
+    blob = payload instanceof myBlob ?
+        payload :
+        new myBlob([payload], {type: mimeType}) ;
+
+
+    function dataUrlToBlob(strUrl) {
+        var parts= strUrl.split(/[:;,]/),
+        type= parts[1],
+        decoder= parts[2] == "base64" ? atob : decodeURIComponent,
+        binData= decoder( parts.pop() ),
+        mx= binData.length,
+        i= 0,
+        uiArr= new Uint8Array(mx);
+
+        for(i;i<mx;++i) uiArr[i]= binData.charCodeAt(i);
+
+        return new myBlob([uiArr], {type: type});
+     }
+
+    function saver(url, winMode){
+
+        if ('download' in anchor) { //html5 A[download]
+            anchor.href = url;
+            anchor.setAttribute("download", fileName);
+            anchor.className = "download-js-link";
+            anchor.innerHTML = "downloading...";
+            anchor.style.display = "none";
+            document.body.appendChild(anchor);
+            setTimeout(function() {
+                anchor.click();
+                document.body.removeChild(anchor);
+                if(winMode===true){setTimeout(function(){ self.URL.revokeObjectURL(anchor.href);}, 250 );}
+            }, 66);
+            return true;
+        }
+
+        // handle non-a[download] safari as best we can:
+        if(/(Version)\/(\d+)\.(\d+)(?:\.(\d+))?.*Safari\//.test(navigator.userAgent)) {
+            if(/^data:/.test(url))	url="data:"+url.replace(/^data:([\w\/\-\+]+)/, defaultMime);
+            if(!window.open(url)){ // popup blocked, offer direct download:
+                if(confirm("Displaying New Document\n\nUse Save As... to download, then click back to return to this page.")){ location.href=url; }
+            }
+            return true;
+        }
+
+        //do iframe dataURL download (old ch+FF):
+        var f = document.createElement("iframe");
+        document.body.appendChild(f);
+
+        if(!winMode && /^data:/.test(url)){ // force a mime that will download:
+            url="data:"+url.replace(/^data:([\w\/\-\+]+)/, defaultMime);
+        }
+        f.src=url;
+        setTimeout(function(){ document.body.removeChild(f); }, 333);
+
+    }//end saver
+
+
+
+
+    if (navigator.msSaveBlob) { // IE10+ : (has Blob, but not a[download] or URL)
+        return navigator.msSaveBlob(blob, fileName);
+    }
+
+    if(self.URL){ // simple fast and modern way using Blob and URL:
+        saver(self.URL.createObjectURL(blob), true);
+    }else{
+        // handle non-Blob()+non-URL browsers:
+        if(typeof blob === "string" || blob.constructor===toString ){
+            try{
+                return saver( "data:" +  mimeType   + ";base64,"  +  self.btoa(blob)  );
+            }catch(y){
+                return saver( "data:" +  mimeType   + "," + encodeURIComponent(blob)  );
+            }
+        }
+
+        // Blob but not URL support:
+        reader=new FileReader();
+        reader.onload=function(e){
+            saver(this.result);
+        };
+        reader.readAsDataURL(blob);
+    }
+    return true;
+};
 
 function getAddressFirstByte(pub) {
     let firstByte = keccak('keccak256').update(Buffer.from(pub)).digest().slice(-20)[0];
